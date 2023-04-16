a2037428dd10ca3f599c60b06d792be2d73818bb
Gauthier Bienvenu <126828223+BreizhBienv@users.noreply.github.com>
2023-04-15 22:34:15 -0400

    First version of portfolio
    Things to add:
    button to the footer to return to the top,
    adapte to screen size,
    add video to projects,

---
+++ assets/js/scripts.js
@@ -0,0 +1,38 @@
+const openModalinputs = document.querySelectorAll('[data-modal-target]')
+const closeModalinputs = document.querySelectorAll('[data-close-button]')
+const blackened = document.getElementById('blackened')
+
+openModalinputs.forEach(input => {
+    input.addEventListener('click', () => {
+        const modal = document.querySelector(input.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+blackened.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalinputs.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal)
+{
+    if (modal == null) return
+    modal.classList.add('active')
+    blackened.classList.add('active')
+}
+
+function closeModal(modal)
+{
+    if (modal == null) return
+    modal.classList.remove('active')
+    blackened.classList.remove('active')
+}
